988a7bf2bc38c1a59f6d10248c6225974c63949f
victorm315 <victorm315@163.com>
2020-08-09 16:47:30 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         # 加个注释，测试Jenkins PCM
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('foo'.upper(), 'FOOx')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
